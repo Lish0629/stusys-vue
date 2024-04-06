0b3f92aac7531484627be9c7d709908dbfb1eb57
Shenghao Li <li.sh0629@outlook.com>
2024-04-06 16:36:04 +0800

    axios和表格联动，以及生命周期的函数

--- src/components/home/Grade.vue
+++ src/components/home/Grade.vue
@@ -1,29 +1,59 @@
 <template>
-  <h1>grade</h1>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
-  </el-table>
-  <el-button @click="showAlert">test</el-button>
+  <div>
+    <h1>Grade</h1>
+    <el-table :data="tableData.data" style="width: 100%">
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+// 使用 reactive 创建响应式对象
+// 使用 onMounted 生命周期函数
+import { reactive,onMounted } from 'vue'; 
+import axios from 'axios';
+
 export default {
-  name: 'Grace',
-  methods:{
-    showAlert() {  
-      axios.get('/api/user')
-      .then(function (response) {
-      console.log(response);
-      // 如果 responseData 是一个对象或数组，使用 JSON.stringify 打印其内容
-      alert(JSON.stringify(response.data, null, 2)); 
-      })
-      .catch(function (error) {
-      console.log(error);
-      });
-    }
+  name: 'Grade',
+  setup() {
+    // 使用 reactive 创建响应式对象
+    const tableData = reactive({
+      data: []
+    });
+
+    onMounted(() => {
+      fetchData();
+    });
+
+    //获取数据的方法
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/user');
+        // 更新响应式对象中的数据
+        // 如果只有一行代码的时候，不会被vue认为是array，所以需要特殊处理
+        // 观察结构发现应该赋值data.data
+        tableData.data = Array.isArray(response.data.data) ? response.data.data : [response.data.data];
+        console.log(tableData);
+      } catch (error) {
+        console.error(error);
+        alert('请求失败: ' + error.message);
+      }
+    };
+
+    //测试代码
+    const showAlert = () => {
+      console.log(tableData);
+      alert(JSON.stringify(tableData, null, 2));
+    };
+
+    // 返回需要在模板中使用的响应式对象和方法
+    return {
+      tableData,
+      fetchData,
+      showAlert
+    };
   }
-}
+};
 </script>
--- src/mock/data.js
+++ src/mock/data.js
@@ -1,5 +1,18 @@
 import Mock from 'mockjs'
+const template = {
+    'date|+1': ['date01', 'date02', 'date03'], // 这里可以是你需要的日期模板
+    'name|+1': ['name123', 'name456', 'name789'], // 这里可以是你需要的姓名模板
+    'address|+1': ['123 Street, City', '456 Avenue, Town', '789 Road, Village'] // 这里可以是你需要的地址模板
+};
+//定义一个mockjs的随机结构
+
+const data = Mock.mock({
+'data|100': [template]
+});
+// 生成10个随机数据
+
 Mock.mock('/api/user', {
-    "data": "data01",
-    "name":"name123"
+    'status': 200, // HTTP 状态码  
+    'message': 'success', // 自定义的消息
+    'data':data.data //数据
 });

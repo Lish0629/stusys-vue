28b7eb09d33a28f072435a3f1b55ad4ecd3a153c
Shenghao Li <li.sh0629@outlook.com>
2024-04-06 20:57:06 +0800

    学生管理

--- src/components/home/Grade.vue
+++ src/components/home/Grade.vue
@@ -2,9 +2,12 @@
   <div>
     <h1>Grade</h1>
     <el-table :data="tableData.data" style="width: 100%">
-      <el-table-column prop="name" label="Name" width="180" />
-      <el-table-column prop="date" label="Date" width="180" />
-      <el-table-column prop="address" label="Address" />
+      <el-table-column prop="id" label="Id" style="width: 15%" />
+      <el-table-column prop="name" label="Name" style="width: 15%" />
+      <el-table-column prop="gender" label="Gender" style="width: 15%" />
+      <el-table-column prop="age" label="Age" style="width: 15%" />
+      <el-table-column prop="phone" label="Phone" style="width: 15%" />
+      <el-table-column prop="classid" label="classid" />
     </el-table>
   </div>
 </template>
--- src/mock/data.js
+++ src/mock/data.js
@@ -5,9 +5,16 @@ const template = {
     'address|+1': ['123 Street, City', '456 Avenue, Town', '789 Road, Village'] // 这里可以是你需要的地址模板
 };
 //定义一个mockjs的随机结构
-
+const student = {
+    'id|+1': ['20240101', '20240102', '20240103'], // 这里可以是你需要的日期模板
+    'name|+1': ['Jack', 'Tom', 'Shelly'], // 这里可以是你需要的姓名模板
+    'gender|+1': ['1', '0', '1'], // 这里可以是你需要的地址模板
+    'age|+1': ['18', '20', '19'],
+    'phone|+1': ['1888888888', '16640404040', '13845645699'],
+    'classid|+1': ['1', '0', '1'] 
+};
 const data = Mock.mock({
-'data|100': [template]
+'data|100': [student]
 });
 // 生成10个随机数据
 

25b3ee2bcb6c23dc8f9bcfed954a0a621cd305f9
Shenghao Li <li.sh0629@outlook.com>
2024-05-02 19:10:12 +0800

    默认地图调用完成

---
+++ src/components/MapView/Map.vue
@@ -0,0 +1,59 @@
+<template>
+  <h1>Map</h1>
+  <div id="map"></div>
+  <link rel="stylesheet" href="https://js.arcgis.com/4.29/esri/themes/light/main.css">
+</template>
+
+
+<script setup>
+import { onMounted } from 'vue'
+import Map from '@arcgis/core/Map.js'
+import MapView from '@arcgis/core/views/MapView.js'
+
+
+
+const apiKey="AAPKa8e2d506cec146d3a6b60637e13c2073m2lceDibabKg5Js9MSf43_-jxN0hymP9WLJzm3KLrDVStqwgnzr7coB5VCityoZE"
+const basemapEnum = "arcgis/streets";
+
+const initMap = () => {
+  const map = new Map({
+    basemap: 'osm'
+  });
+
+  const view = new MapView({
+    map: map,
+    container: 'map',
+    center: [120.205, 30.248],
+    zoom: 13
+  });
+
+  setTimeout(() => {
+    view.goTo({
+      center: [104.061982, 30.577203],
+      zoom: 15
+    })
+  }, 3000);
+
+  view.ui.components=[]
+};
+
+
+
+onMounted(initMap);
+</script>
+
+<style scoped>
+#map {
+  height:80vh;
+  width:100%;
+  border-radius:0px;
+  border: 2px solid #000;
+}
+
+html,body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
